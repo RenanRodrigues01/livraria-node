965fb7937f852211a3aabc232f929ab032785ccc
renan <renan.rodriguesmoura2022@gmail.com>
2022-12-16 20:23:23 -0300

    configurando cors

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import db from "./config/dbConnect.js";
 import routes from "./routes/index.js";
+import cors from 'cors'
 
 //estabelece a conexão com o banco de dados
 db.on("error", console.log.bind(console, "erro de conexão"));
@@ -12,6 +13,12 @@ db.once("open", () => {
 const app = express();
 //converte os arquivos para formato JSON
 app.use(express.json());
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    app.use(cors());
+    next();
+})
 // estabelece as rotas com o arquivo routes passando o proprio app como parametro
 routes(app)
 
