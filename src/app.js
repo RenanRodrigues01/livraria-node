78e7c714c89fe02ef4e106f379ecfb1242d8f2c4
renan <renan.rodriguesmoura2022@gmail.com>
2022-12-17 18:09:10 -0300

    corrigindo erro na configiração cors

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ app.use(express.json());
 // responsavel por conceder acesso de outras urls a API
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
+    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST');
     app.use(cors());
     next();
 })
